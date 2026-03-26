765ed131dc3e8637f7511770c694844d514f8841
David Capo-chichi <david.capo-chichi@epitech.eu>
2026-03-26 09:03:08 +0100

    fix coding-style error

---
+++ src/my_sh_3.c
@@ -0,0 +1,53 @@
+/*
+** EPITECH PROJECT, 2026
+** minishell1
+** File description:
+** description
+*/
+
+#include "../include/mysh.h"
+#include "../include/librog.h"
+void add_to_history(char *cmd)
+{
+    int fd = open(".history", O_WRONLY | O_APPEND | O_CREAT, 0644);
+
+    if (fd == -1)
+        return;
+    write(fd, cmd, my_strlen(cmd));
+    close(fd);
+}
+
+void display_history(void)
+{
+    int fd = open(".history", O_RDONLY);
+    struct stat data;
+    char *buf;
+    ssize_t len;
+    int val;
+
+    val = stat(".history", &data);
+    if (fd == -1 || val == -1)
+        return;
+    buf = malloc(sizeof(data.st_size));
+    if (!buf) {
+        close(fd);
+        return;
+    }
+    len = (ssize_t) data.st_size;
+    if (read(fd, buf, len) > 0)
+        my_printf("%s", buf);
+    close(fd);
+}
+
+int exec_builtin_3(my_sh_t *args, control_t *ctrl)
+{
+    if (!my_strcmp(args->cmd_array[0], "unsetenv")) {
+        x_unsetenv(&ctrl->env, args->cmd_array);
+        return 1;
+    }
+    if (!my_strcmp(args->cmd_array[0], "history")) {
+        display_history();
+        return 1;
+    }
+    return 0;
+}
